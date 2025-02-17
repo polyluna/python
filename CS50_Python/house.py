d3ee5b0d8005fa6125cef164d9a190aee786bd50
ployphusri <ployphusri@gmail.com>
2025-02-17 22:37:59 +0700

    add content to house and parity

--- CS50_Python/house.py
+++ CS50_Python/house.py
@@ -1,1 +1,9 @@
-#test
+name = input ("What's your name? ")
+
+match name:
+        case "Harry" | "Hermione":
+                print("Griffindor")
+        case "Draco":
+                print("Slytherin")
+        case _:
+                print("who?")
--- CS50_Python/parity.py
+++ CS50_Python/parity.py
@@ -1,1 +1,12 @@
-#test
+def main():
+        x = int(input("Please enter number"))
+        print(x)
+        if is_even(x):
+                print ("even")
+        else:
+                print ("odd")
+
+def is_even(n):
+        return (n % 2 == 0)
+
+main()
